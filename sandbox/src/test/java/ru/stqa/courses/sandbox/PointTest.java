f37a11dbf1f7ecd77f3fe8dfb3da4e2f472b9fa3
y-ozza <yoozza@yandex.ru>
2017-06-16 16:29:07 -0400

    Задание 3

--- sandbox/src/test/java/ru/stqa/courses/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/courses/sandbox/PointTest.java
@@ -1,13 +1,17 @@
 package ru.stqa.courses.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
 
 /**
  * Created by shurik on 13.06.2017.
  */
 public class PointTest {
    @Test
-   public  void  testDistanceBetween() {
-
+   public  void  testDistanceTo() {
+      Point point1 = new Point(2.0, 2.0);
+      Point point2 = new Point(5.0, 6.0);
+      Assert.assertEquals(point1.distanceTo(point2), 5.0);
    }
 }
