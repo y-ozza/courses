254b72fe0bb015e0f3511862fa028c89ce08891e
y-ozza <yoozza@yandex.ru>
2017-06-26 13:08:49 -0400

    Проблема с модификацией групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -9,7 +9,7 @@ import ru.stqa.pft.addressbook.model.GroupData;
 public class GroupModificationTest extends  TestBase{
 
    @Test
-   public void testGroupCreation() {
+   public void testGroupModification() {
       app.getNavigationHelper().gotoGroupPage();
       app.getGroupHelper().initGroupModification();
       app.getGroupHelper().fillGroupForm(new GroupData("test11", "test22", "test33"));
